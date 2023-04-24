06012ca540177b98bfbd757423097acce650853a
Sheila Seruya <Sheilaseruya@gmail.com>
2023-04-24 07:04:39 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function handleFocus(event){
+         console.log("Good!")
+    }
+    function handleBlure(event){
+         console.log("Hey! Eyes on me!")
+    }
+    return(
+        <button onFocus={handleFocus} onBlur={handleBlure}>Eyes on me</button>
+    );
+}
+
+export default EyesOnMe;
